c54d9fa016b55e0f90657793b9f77933315e0e72
reissGRVS <rwag365@gmail.com>
2017-07-10 23:43:40 +0100

    meh

---
+++ app/src/main/java/com/reissgrvs/spotifyplaylisttool/Util/MyPlaylistsStore.java
@@ -0,0 +1,16 @@
+package com.reissgrvs.spotifyplaylisttool.Util;
+
+import java.util.List;
+
+import kaaes.spotify.webapi.android.models.PlaylistSimple;
+
+/**
+ * Created by Reiss on 10/07/2017.
+ */
+
+public class MyPlaylistsStore {
+    //TODO: Fucking get rid of this you moron
+    static public  List<PlaylistSimple> myPlaylists;
+}
+
+
